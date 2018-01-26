f96836aca3e90de8091446dae6fee1be1d12e8f9
dannyellis <dannyellis87@gmail.com>
2018-01-26 14:56:17 -0700

    Adding IP restriction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :block_foreign_hosts
+
+  def whitelisted?(ip)
+    return true if [74.3.135.162].include?(ip)
+    false
+  end
+
+  def block_foreign_hosts
+    return false if whitelisted?(request.remote_ip)
+    redirect_to "https://www.google.com" unless request.remote_ip.start_with?("123.456.789")
+  end
 end
